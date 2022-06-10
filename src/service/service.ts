b35faebfa7ce17b58199f76494b1c30e4865d998
Nanpipat🐻 <top@dept.email>
2022-06-10 16:40:49 +0700

    time less

--- src/service/service.ts
+++ src/service/service.ts
@@ -14,65 +14,68 @@ export async function getCookieService(url: string): Promise<CookiesType[]> {
           ]
         })
 
-        const page = await browser.newPage();
+        const responseCookie = await getCoolies(browser, url)
+        return responseCookie
 
-        // let encoded = encodeURI(url);
-        const withHttp = () => url.replace(/^(?:(.*:)?\/\/)?(.*)/i, (match, schemma, nonSchemmaUrl) => schemma ? match : `https://${nonSchemmaUrl}`);
+      //   const page = await browser.newPage();
 
-        console.log(url,withHttp(),"url")
+      //   // let encoded = encodeURI(url);
+      //   const withHttp = () => url.replace(/^(?:(.*:)?\/\/)?(.*)/i, (match, schemma, nonSchemmaUrl) => schemma ? match : `https://${nonSchemmaUrl}`);
 
-        await page.goto(withHttp());
+      //   console.log(url,withHttp(),"url")
 
-        // const element = await page.waitForTimeout(5000)
+      //   await page.goto(withHttp());
 
-      const findLinks = await page.evaluate(() =>
-        Array.from(document.querySelectorAll("a")).map((info) => ({
-          url: info.href
-        }))
-      );
+      //   // const element = await page.waitForTimeout(5000)
 
-      // const fl: string[] = []
-      // await page.evaluate(() => {
-      //   if(document.querySelectorAll("a")) {
-      //     for(let d =0; (d < document.querySelectorAll("a").length && d < 10); d++){
-      //       console.log(document.querySelectorAll("a")[d])
-      //     }
+      // const findLinks = await page.evaluate(() =>
+      //   Array.from(document.querySelectorAll("a")).map((info) => ({
+      //     url: info.href
+      //   }))
+      // );
+
+      // // const fl: string[] = []
+      // // await page.evaluate(() => {
+      // //   if(document.querySelectorAll("a")) {
+      // //     for(let d =0; (d < document.querySelectorAll("a").length && d < 10); d++){
+      // //       console.log(document.querySelectorAll("a")[d])
+      // //     }
+      // //   }
+      // // })
+
+      // console.log(findLinks?.length,"findLinks?.length")
+
+      // for(let j=0; j< 5 && j < findLinks?.length; j++) {
+      //   if(findLinks[j]?.url){
+      //     console.log(findLinks[j]?.url,"findLinks")
+      //     await page.goto(findLinks[j]?.url);
+      //     // await page.waitForTimeout(1000)
       //   }
-      // })
-
-      console.log(findLinks?.length,"findLinks?.length")
-
-      for(let j=0; j< 5 && j < findLinks?.length; j++) {
-        if(findLinks[j]?.url){
-          console.log(findLinks[j]?.url,"findLinks")
-          await page.goto(findLinks[j]?.url);
-          // await page.waitForTimeout(1000)
-        }
-      }
+      // }
 
 
-        // const element = await page.waitForTimeout(20000)
+      //   // const element = await page.waitForTimeout(20000)
 
-        const cookies = await page.cookies()
+      //   const cookies = await page.cookies()
 
-        console.log(cookies,"cookies")
+      //   console.log(cookies,"cookies")
 
-        const responseCookie: CookiesType[] = []
+      //   const responseCookie: CookiesType[] = []
 
-        if (cookies) {
+      //   if (cookies) {
 
-            for (var i = 0; i < cookies?.length; i++) {
-                const cookietype: CookiesType = {
-                    domain: cookies[i].domain,
-                    value: cookies[i].value,
-                    name: cookies[i].name,
-                }
-                await responseCookie.push(cookietype)
-            }
+      //       for (var i = 0; i < cookies?.length; i++) {
+      //           const cookietype: CookiesType = {
+      //               domain: cookies[i].domain,
+      //               value: cookies[i].value,
+      //               name: cookies[i].name,
+      //           }
+      //           await responseCookie.push(cookietype)
+      //       }
 
-        }
+      //   }
 
-      return responseCookie
+      // return responseCookie
       } else {
         const browser = await chromium.puppeteer.launch({
           args: chromium.args,
@@ -81,81 +84,137 @@ export async function getCookieService(url: string): Promise<CookiesType[]> {
           headless: true,
           ignoreHTTPSErrors: true,
         })
-        const page = await browser.newPage();
 
-         // let encoded = encodeURI(url);
-         const withHttp = () => url.replace(/^(?:(.*:)?\/\/)?(.*)/i, (match, schemma, nonSchemmaUrl) => schemma ? match : `https://${nonSchemmaUrl}`);
+        const responseCookie = await getCoolies(browser, url)
+        return responseCookie
+      //   const page = await browser.newPage();
 
-         console.log(url,withHttp(),"url")
+      //    // let encoded = encodeURI(url);
+      //    const withHttp = () => url.replace(/^(?:(.*:)?\/\/)?(.*)/i, (match, schemma, nonSchemmaUrl) => schemma ? match : `https://${nonSchemmaUrl}`);
+
+      //    console.log(url,withHttp(),"url")
  
-         await page.goto(withHttp());
+      //    await page.goto(withHttp());
  
-        //  const element = await page.waitForTimeout(5000)
+      //   //  const element = await page.waitForTimeout(5000)
  
-      //  const findLinks = await page.evaluate(() =>
-      //    Array.from(document.querySelectorAll("a")).map((info) => ({
-      //      url: info.href
-      //    }))
-      //  );
+      // //  const findLinks = await page.evaluate(() =>
+      // //    Array.from(document.querySelectorAll("a")).map((info) => ({
+      // //      url: info.href
+      // //    }))
+      // //  );
  
-       // const fl: string[] = []
-       // await page.evaluate(() => {
-       //   if(document.querySelectorAll("a")) {
-       //     for(let d =0; (d < document.querySelectorAll("a").length && d < 10); d++){
-       //       console.log(document.querySelectorAll("a")[d])
-       //     }
-       //   }
-       // })
+      //  // const fl: string[] = []
+      //  // await page.evaluate(() => {
+      //  //   if(document.querySelectorAll("a")) {
+      //  //     for(let d =0; (d < document.querySelectorAll("a").length && d < 10); d++){
+      //  //       console.log(document.querySelectorAll("a")[d])
+      //  //     }
+      //  //   }
+      //  // })
  
-      //  console.log(findLinks?.length,"findLinks?.length")
+      // //  console.log(findLinks?.length,"findLinks?.length")
  
-      //  for(let j=0; j< 5 && j < findLinks?.length; j++) {
-      //    if(findLinks[j]?.url){
-      //      console.log(findLinks[j]?.url,"findLinks")
-      //      await page.goto(findLinks[j]?.url);
-      //     //  await page.waitForTimeout(1000)
+      // //  for(let j=0; j< 5 && j < findLinks?.length; j++) {
+      // //    if(findLinks[j]?.url){
+      // //      console.log(findLinks[j]?.url,"findLinks")
+      // //      await page.goto(findLinks[j]?.url);
+      // //     //  await page.waitForTimeout(1000)
+      // //    }
+      // //  }
+ 
+ 
+      //    // const element = await page.waitForTimeout(20000)
+ 
+      //    const cookies = await page.cookies()
+ 
+      //    console.log(cookies,"cookies")
+ 
+      //    const responseCookie: CookiesType[] = []
+ 
+      //    if (cookies) {
+ 
+      //        for (var i = 0; i < cookies?.length; i++) {
+      //            const cookietype: CookiesType = {
+      //                domain: cookies[i].domain,
+      //                value: cookies[i].value,
+      //                name: cookies[i].name,
+      //            }
+      //            await responseCookie.push(cookietype)
+      //        }
+ 
       //    }
-      //  }
  
- 
-         // const element = await page.waitForTimeout(20000)
- 
-         const cookies = await page.cookies()
- 
-         console.log(cookies,"cookies")
- 
-         const responseCookie: CookiesType[] = []
- 
-         if (cookies) {
- 
-             for (var i = 0; i < cookies?.length; i++) {
-                 const cookietype: CookiesType = {
-                     domain: cookies[i].domain,
-                     value: cookies[i].value,
-                     name: cookies[i].name,
-                 }
-                 await responseCookie.push(cookietype)
-             }
- 
-         }
- 
-       return responseCookie
+      //  return responseCookie
+      // }
+      //   // const browser = await puppeteer.launch({
+      //   //   headless: true,
+      //   //   executablePath: '/usr/bin/chromium-browser',
+      //   //   args: [
+      //   //     "--disable-gpu",
+      //   //     "--disable-dev-shm-usage",
+      //   //     "--disable-setuid-sandbox",
+      //   //     "--no-sandbox",
+      //   //   ],
+      //   // });
       }
-        // const browser = await puppeteer.launch({
-        //   headless: true,
-        //   executablePath: '/usr/bin/chromium-browser',
-        //   args: [
-        //     "--disable-gpu",
-        //     "--disable-dev-shm-usage",
-        //     "--disable-setuid-sandbox",
-        //     "--no-sandbox",
-        //   ],
-        // });
        
     } catch (error) {
         console.log(error,"errrr")
       throw new Error("Invalid url");
     }
+  }
 
-    return []
+
+  async function getCoolies(browser: Browser | any, url: string): Promise<CookiesType[]> {
+    const page = await browser.newPage();
+    const withHttp = () => url.replace(/^(?:(.*:)?\/\/)?(.*)/i, (match, schemma, nonSchemmaUrl) => schemma ? match : `https://${nonSchemmaUrl}`);
+    console.log(url, withHttp(), "url")
+  
+    await page.goto(withHttp());
+  
+    const findLinks = await page.evaluate(() => {
+      console.log(document.querySelectorAll("a"), "href")
+      let arr = []
+      if (document.querySelectorAll("a")) {
+        for (let i = 0; i < document.querySelectorAll("a").length && i < 5; i++) {
+          console.log(document.querySelectorAll("a")[i].href, "href")
+          arr.push(document.querySelectorAll("a")[i].href)
+        }
+  
+        return arr
+      }
+    })
+  
+    console.log(findLinks, "findLinks")
+  
+    for (let j = 0; j < 5 && j < (findLinks as string[]).length; j++) {
+      if ((findLinks as string[])[j]) {
+        console.log((findLinks as string[])[j], "findLinks")
+        await page.goto((findLinks as string[])[j]);
+        //  await page.waitForTimeout(1000)
+      }
+    }
+  
+    const client = await page.target().createCDPSession();
+    const cookies = (await client.send('Network.getAllCookies')).cookies;
+  
+    console.log(cookies, "cookies")
+  
+    const responseCookie: CookiesType[] = []
+  
+    if (cookies) {
+  
+      for (var i = 0; i < cookies?.length; i++) {
+        const cookietype: CookiesType = {
+          domain: cookies[i].domain,
+          value: cookies[i].value,
+          name: cookies[i].name,
+        }
+        await responseCookie.push(cookietype)
+      }
+  
+    }
+  
+    return responseCookie
   }
