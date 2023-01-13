c82212d996827052da22d1d5599722d24de2f1d4
Nanpipat Klinpratoom <top.na@finema.co>
2023-01-13 14:32:46 +0700

    add cors

--- build/server.js
+++ build/server.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const controller_1 = require("./controller/controller");
 const host = '0.0.0.0';
 const port = parseInt(process.env.PORT, 10) || 5055;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.get("/api/healthcheck", (req, res) => res.status(200).send("ok"));
+app.get("/api/healthz", (req, res) => res.status(200).send("ok"));
 app.get("/api/cookies", controller_1.getCookie);
 app.post("/api/company", controller_1.saveCompany);
 app.listen(port, host, function () {
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import log from './logger'
 import routes from './routes';
 import {getCookie, saveCompany} from "./controller/controller"
@@ -10,6 +11,7 @@ const port: number = parseInt(<string>process.env.PORT, 10) || 5055
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 
 app.get("/api/healthz", (req: Request, res: Response) => res.status(200).send("ok"));
